baf37aad82d577607d07c31c76059f29e593ba17
carltonupperdine <carlton.upperdine@gmail.com>
2023-02-25 11:00:36 +0000

    scaffold project

---
+++ src/components/Layout.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { PropsWithChildren } from "react";
+
+
+export default function Layout ({ children } : PropsWithChildren) {
+    return (
+        <div className="flex flex-col min-h-screen font-mono">
+            <header className="bg-sky-900 mb-8 py-4">
+                <div className="container mx-auto flex justify-center text-white text-xl">
+                    <Link href="/">
+                        <>Carlton Upperdine</>
+                    </Link>
+                </div>
+            </header>
+            <main className="container mx-auto flex-1">{children}</main>
+            <footer className="bg-sky-900 mt-8 py-4">
+                <div className="container mx-auto flex justify-center text-white">
+                    &copy; 2023 Carlton Upperdine
+                </div>
+            </footer>
+        </div>
+    );
+}
