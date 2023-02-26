819d5dd5be16121c25feaa8f0d8116f34396d100
carltonupperdine <carlton.upperdine@gmail.com>
2023-02-26 12:19:01 +0000

    update company descriptions

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -57,20 +57,20 @@ export default function About() {
     const jobs: Experience[] = [
         {
             company: "BJSS",
-            description: `BJSS is the leading technology and engineering consultancy 
-                for business. Trusted by our clients, we collaborate with some of the 
-                world’s leading organisations to deliver complex, innovative technology, 
-                engineering, and industry solutions that millions of people use every day.`,
+            description: `Working as a consultant for a variety of industries and 
+                clients. Most recently I have been working on a large scale digital 
+                transformation program for a leading British online gambling company.`,
             logo: "/companies/bjss.svg",
             start: "September 2022",
             title: "Senior Software Engineer"
         },
         {
             company: "Smoothwall",
-            description: `Smoothwall is the leading digital safeguarding solutions provider 
-                in UK Education. 12,500 schools, colleges and multi-academy trusts 
-                depend on our technologies to keep their students safe and their 
-                education organisations compliant.`,
+            description: `Working as the technical lead for the company's Cloud Filter 
+                solution, I was tasked with evolving the team's engineering practices 
+                as the application scaled. At peak our system hit approximately 250k 
+                devices using our solution, and the critical nature of our product meant
+                that stability was crucial.`,
             logo: "/companies/smoothwall.svg",
             start: "May 2021",
             end: "May 2022",
@@ -79,29 +79,25 @@ export default function About() {
         },
         {
             company: "3Squared",
-            description: `
-            At 3Squared, we design, develop and commission integrated software solutions for some of the UKs biggest brands within rail and construction.
-
-            Our teams collaborate across disciplines to provide integrated, compelling multiplatform software solutions that deliver value to our customers.
-            
-            We have a powerful, flexible and integrated software platform designed to support the operations of today’s modern rail operators and support the Digital Railway.
-            `,
+            description: `Worked on a number of products under the 3Squared banner, developing features 
+                and fixing bugs. Outside of my product work, I built a number of internal libraries used
+                by the wider team including a framework for creating ASP.NET Core health checks. I also 
+                temporarily took charge of the company's cloud estate in Azure, utilising IaC tooling such
+                as Terraform to provision and manage resources, as well as offering guidance on 
+                architectural decisions.`,
             logo: "/companies/3squared.svg",
             start: "January 2019",
             end: "May 2021",
             title: "Software Engineer"
         },
         {
-            company: "MAM Software (acquired by KCS)",
-            description: `
-            Kerridge Commercial Systems (KCS), the parent 
-            company of MAM Software, provides specialised 
-            software, services, and support to deliver fully 
-            integrated trading and business management solutions 
-            to companies in the distributive trades across the world.
-            `,
+            company: "MAM Software",
+            description: `Predominantly working on a SaaS product used by garages across the UK and North America
+                to manage their bookings. I added a number of features to the application, as well as being part of
+                a massive rewrite of core parts of the application using React and TypeScript. Prior to this role, 
+                I worked as a QA Engineer, and used Selenium WebDriver to create automated tests using C#.`,
             logo: "companies/MAM.svg",
-            start: "May 2016",
+            start: "May 2011",
             end: "December 2018",
             title: "Software Engineer"
         }
