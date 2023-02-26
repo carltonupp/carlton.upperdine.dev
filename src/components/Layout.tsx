399260f440183f0558ea3404d39de150a5677a0e
carltonupperdine <carlton.upperdine@gmail.com>
2023-02-26 10:33:20 +0000

    responsive navigation

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -9,10 +9,22 @@ export default function Layout ({ children } : PropsWithChildren) {
     return (
         <div className="flex flex-col min-h-screen font-mono">
             <header className="bg-sky-900 mb-8 py-4">
-                <div className="container mx-auto flex justify-center text-white text-xl">
-                    <Link href="/">
+                <div className="container mx-auto text-center md:text-left md:flex justify-center text-white text-xl">
+                    <div className="basis-1/2">
                         <>Carlton Upperdine</>
-                    </Link>
+                    </div>
+                    <div>
+                        <Link href="/" className="basis-1/2 mr-4">
+                            <>Home</>
+                        </Link>
+                        <Link href="/about" className="mr-4">
+                            <>About</>
+                        </Link>
+                        <Link href="/posts">
+                            <>Posts</>
+                        </Link>
+                    </div>
+
                 </div>
             </header>
             <main className="container mx-auto flex-1">
---
+++ src/pages/posts.tsx
@@ -0,0 +1,8 @@
+export default function Posts() {
+    return (
+        <div className="container">
+            <h1 className="flex justify-center text-2xl">Posts</h1>
+            <p className="flex justify-center">No posts yet - check in soon!</p>
+      </div>
+    )
+}
