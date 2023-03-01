1ba37e01f3a84ebd93151de3a11c9cd68562a2a1
carltonupperdine <carlton.upperdine@gmail.com>
2023-03-01 20:22:16 +0000

    fix about page layout on mobile

--- src/components/Job.tsx
+++ src/components/Job.tsx
@@ -1,18 +1,25 @@
 import { Experience } from "@/shared/models";
-import Image from "next/image"
+import Image from "next/image";
 import styles from "../styles/about.module.scss";
 
-export default function Job(props: {job: Experience}) {
-    return (
-        <div className="flex m-5">
-                <Image src={props.job.logo} 
-                    alt="company logo" height={100} width={100} className={styles.companyImage + " mr-5"}></Image>
-                <div>
-                    <h3 className="text-lg font-bold">{props.job.title}</h3>
-                    <p className="text-sm font-bold">{props.job.company}</p>
-                    <p className="text-sm font-semibold">{props.job.start} - {props.job.end ?? 'Present'}</p>
-                    {props.job.description}
-                </div>
-        </div>
-    )
+export default function Job(props: { job: Experience }) {
+  return (
+    <div className="flex my-5">
+      <Image
+        src={props.job.logo}
+        alt="company logo"
+        height={100}
+        width={100}
+        className={styles.companyImage + " mr-5"}
+      ></Image>
+      <div>
+        <h3 className="text-lg font-bold">{props.job.title}</h3>
+        <p className="text-sm font-bold">{props.job.company}</p>
+        <p className="text-sm font-semibold">
+          {props.job.start} - {props.job.end ?? "Present"}
+        </p>
+        {props.job.description}
+      </div>
+    </div>
+  );
 }
