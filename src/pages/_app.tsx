8bdba2ed4049afc0664b05235fe97ac50bcc4e26
carltonupperdine <carlton.upperdine@gmail.com>
2023-03-01 22:30:13 +0000

    add scripts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,31 @@
-import Layout from '@/components/Layout';
-import '@/styles/globals.scss'
-import type { AppProps } from 'next/app'
+import Layout from "@/components/Layout";
+import "@/styles/globals.scss";
+import type { AppProps } from "next/app";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-WGK06NVVVH"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+      window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-WGK06NVVVH');
+      `,
+        }}
+      ></Script>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
