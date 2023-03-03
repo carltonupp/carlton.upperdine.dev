9c00c40390c4652679995ebd92f6ad4ede6922b2
carltonupperdine <carlton.upperdine@gmail.com>
2023-03-03 22:43:16 +0000

    add google-analytics code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,28 +1,12 @@
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 import Layout from "@/components/Layout";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
-import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Script
-        strategy="afterInteractive"
-        src="https://www.googletagmanager.com/gtag/js?id=G-WGK06NVVVH"
-      />
-      <Script
-        id="google-analytics"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-      window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', 'G-WGK06NVVVH');
-      `,
-        }}
-      ></Script>
+      <GoogleAnalytics />
       <Layout>
         <Component {...pageProps} />
       </Layout>
