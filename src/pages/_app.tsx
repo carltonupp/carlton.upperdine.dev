b70a52159b99c731631a56de2124e6b576b52820
carltonupperdine <carlton.upperdine@gmail.com>
2023-03-03 23:30:58 +0000

    working for sure this time

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,24 @@
-import GoogleAnalytics from "@/components/GoogleAnalytics";
 import Layout from "@/components/Layout";
 import "@/styles/globals.scss";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as gtag from "../lib/gtag";
+const isProduction = process.env.NODE_ENV === "production";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      if (isProduction) gtag.pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       <Layout>
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,7 @@
-import GoogleAnalytics from "@/components/GoogleAnalytics";
 import { Html, Head, Main, NextScript } from "next/document";
+import { GA_TRACKING_ID } from "../lib/gtag";
+
+const isProduction = process.env.NODE_ENV === "production";
 
 export default function Document() {
   return (
@@ -7,8 +9,29 @@ export default function Document() {
       <Head>
         <meta name="keywords" content="title, meta, nextjs" />
         <meta name="author" content="Carlton Upperdine" />
+        {/* enable analytics script only for production */}
+        {isProduction && (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+            />
+            <script
+              // eslint-disable-next-line react/no-danger
+              dangerouslySetInnerHTML={{
+                __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+              }}
+            />
+          </>
+        )}
       </Head>
-      <GoogleAnalytics />
       <body>
         <Main />
         <NextScript />
