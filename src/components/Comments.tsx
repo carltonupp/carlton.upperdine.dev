e8d4881d7a20536318b8fdd48fb27d66f2922185
carltonupperdine <carlton.upperdine@gmail.com>
2023-03-04 13:06:44 +0000

    add debug console to check env var is being read

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,9 +1,9 @@
 import { BlogPostMetadata } from "@/shared/models";
 import { DiscussionEmbed } from "disqus-react";
-const isProduction = process.env.NODE_ENV === "production";
 
 export default function Comments(props: { post: BlogPostMetadata }) {
   const disqusShortname = process.env.NEXT_PUBLIC_DISQUS_SHORTNAME ?? "";
+  console.log(disqusShortname);
   const disqusConfig = {
     url: `https://carlton.upperdine.dev/post/${props.post.slug}`,
     identifier: props.post.slug,
