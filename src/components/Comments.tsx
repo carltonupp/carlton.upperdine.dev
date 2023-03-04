cc3d403f72c67836e87eb74493e138de01e099d1
carltonupperdine <carlton.upperdine@gmail.com>
2023-03-04 13:08:52 +0000

    remove console

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -3,7 +3,6 @@ import { DiscussionEmbed } from "disqus-react";
 
 export default function Comments(props: { post: BlogPostMetadata }) {
   const disqusShortname = process.env.NEXT_PUBLIC_DISQUS_SHORTNAME ?? "";
-  console.log(disqusShortname);
   const disqusConfig = {
     url: `https://carlton.upperdine.dev/post/${props.post.slug}`,
     identifier: props.post.slug,
