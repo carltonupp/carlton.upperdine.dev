af38826145152995440d646eeceb822e31dd5992
carltonupperdine <carlton.upperdine@gmail.com>
2023-04-23 12:31:16 +0100

    add tests for profile component

---
+++ test/components/profile.spec.tsx
@@ -0,0 +1,20 @@
+import Profile from "../../src/components/Profile";
+import "@testing-library/jest-dom";
+import { RenderResult, render } from "@testing-library/react";
+
+describe("Profile component", () => {
+  let body: RenderResult;
+
+  beforeEach(() => {
+    body = render(<Profile />);
+  });
+
+  it("has a display picture", () => {
+    const image = body.getByAltText(
+      "me enjoying the scenery of Zakynthos town"
+    );
+    expect(image).toBeInTheDocument();
+
+    expect(image.getAttribute("src")).toContain("pfp.jpg");
+  });
+});
