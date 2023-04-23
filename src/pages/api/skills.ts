26e5bfdb3e3d853dcc5e88d9391d8470ddb75b07
carltonupperdine <carlton.upperdine@gmail.com>
2023-04-23 10:52:04 +0100

    updated for recent career change

--- src/pages/api/skills.ts
+++ src/pages/api/skills.ts
@@ -3,59 +3,59 @@ import { NextApiRequest, NextApiResponse } from "next";
 import jobs from "./jobs";
 
 const skills: Skill[] = [
-    {
-      technology: "C#",
-      level: 100,
-    },
-    {
-      technology: "TypeScript",
-      level: 95,
-    },
-    {
-      technology: "Microsoft Azure",
-      level: 90,
-    },
-    {
-      technology: "Angular",
-      level: 90,
-    },
-    {
-      technology: "Terraform",
-      level: 75,
-    },
-    {
-      technology: "React",
-      level: 80,
-    },
-    {
-      technology: "Go",
-      level: 60,
-    },
-    {
-      technology: "Java/Kotlin",
-      level: 75,
-    },
-    {
-      technology: "Spring Boot",
-      level: 75,
-    },
-    {
-      technology: "ASP.NET Core",
-      level: 95,
-    },
-    {
-      technology: "Node.js",
-      level: 80,
-    },
-    {
-      technology: "SQL Server",
-      level: 70,
-    },
-  ];
+  {
+    technology: "C#",
+    level: 100,
+  },
+  {
+    technology: "TypeScript",
+    level: 95,
+  },
+  {
+    technology: "Microsoft Azure",
+    level: 90,
+  },
+  {
+    technology: "Angular",
+    level: 90,
+  },
+  {
+    technology: "Terraform",
+    level: 75,
+  },
+  {
+    technology: "React",
+    level: 90,
+  },
+  {
+    technology: "Go",
+    level: 60,
+  },
+  {
+    technology: "Java/Kotlin",
+    level: 80,
+  },
+  {
+    technology: "Spring Boot",
+    level: 80,
+  },
+  {
+    technology: "ASP.NET Core",
+    level: 95,
+  },
+  {
+    technology: "Node.js",
+    level: 85,
+  },
+  {
+    technology: "SQL Server",
+    level: 70,
+  },
+];
 
 export default function handler(
-    req: NextApiRequest,
-    res: NextApiResponse<Skill[]>
-  ) {
-    res.status(200).json([...skills])
-  }
+  req: NextApiRequest,
+  res: NextApiResponse<Skill[]>
+) {
+  res.status(200).json([...skills]);
+}
